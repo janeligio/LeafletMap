986a2f52b74080573f70625155f02c7b14091386
janeligio <jeligio808@gmail.com>
2024-09-01 19:43:35 -1000

    Checkpoint.

--- LeafletMap.js
+++ LeafletMap.js
@@ -1,22 +1,104 @@
 define([
     'qlik',
-    'text!./template.html',
+    'jquery',
     './properties',
     './leaflet',
+    'text!./template.html',
     'css!./styles.css',
     'css!./leaflet.css',
-], function (qlik, template, properties, L) {
+], function (qlik, $, properties, L, template) {
+    'use strict';
+
+    function render(layout) {
+        // Get the user-defined default view coordinates
+        const defaultLat = layout.defaultViewLat;
+        const defaultLong = layout.defaultViewLong;
+
+        // Initialize the map with user-defined coordinates
+        const map = L.map('leaflet-map').setView([defaultLat, defaultLong], 13);
+
+        // Add a tile layer to the map (using OpenStreetMap tiles)
+        // 'https://maps.qlikcloud.com/ravegeo/map2/GetTile?name=osm_bg.24.4&tx={x}&ty={y}&zoomStep={z}&key=may21-K8DU2D3jU9PWhr',
+        // https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+        L.tileLayer(
+            'https://maps.qlikcloud.com/ravegeo/map2/GetTile?name=osm_bg.24.4&tx={x}&ty={y}&zoomStep={z}&key=may21-K8DU2D3jU9PWhr',
+            {
+                attribution:
+                    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            }
+        ).addTo(map);
+
+        // Iterate over the user-defined layers
+        // if (layout.layers) {
+        //     layout.layers.forEach(function (layer) {
+        //         // Determine the layer type
+        //         var layerType = layer.layerType;
+        //         var dimension =
+        //             layout.qHyperCube.qDimensionInfo[layer.dimension]
+        //                 .qFallbackTitle;
+
+        //         // Example: Add different types of layers based on the user selection
+        //         if (layerType === 'marker') {
+        //             // Assuming the dimension provides coordinates
+        //             var latLng = layout.qHyperCube.qDataPages[0].qMatrix.map(
+        //                 function (d) {
+        //                     return d[layer.dimension].qText
+        //                         .split(',')
+        //                         .map(Number);
+        //                 }
+        //             );
+
+        //             latLng.forEach(function (coords) {
+        //                 L.marker(coords)
+        //                     .addTo(map)
+        //                     .bindPopup(
+        //                         '<b>' +
+        //                             dimension +
+        //                             '</b><br/>Lat: ' +
+        //                             coords[0] +
+        //                             ', Long: ' +
+        //                             coords[1]
+        //                     );
+        //             });
+        //         }
+        //         // Additional layer types can be handled here
+        //     });
+        // }
+    }
+
     return {
+        template: template,
+        initialProperties: {
+            qHyperCubeDef: {
+                qDimensions: [],
+                qMeasures: [],
+                qInitialDataFetch: [
+                    {
+                        qWidth: 0,
+                        qHeight: 0,
+                    },
+                ],
+            },
+        },
         definition: properties,
-        template,
         support: {
             snapshot: true,
             export: true,
             exportData: true,
         },
-        paint: function () {
+        paint: function ($element, layout) {
+            console.log(layout);
             return qlik.Promise.resolve();
         },
-        controller: ['$scope', function ($scope) {}],
+        controller: [
+            '$scope',
+            function ($scope) {
+                console.log($scope.layout);
+
+                $(document).ready(() => {
+                    render($scope.layout);
+                });
+            },
+        ],
     };
 });
--- properties.js
+++ properties.js
@@ -1,25 +1,93 @@
 define(['qlik'], function (qlik) {
+    const layers = {
+        type: 'array',
+        label: 'Layers',
+        ref: 'layers',
+        itemTitleRef: 'layerType',
+        allowAdd: true,
+        allowRemove: true,
+        addTranslation: 'Add Layer',
+        itemTitleRef: 'layerTitle',
+        items: {
+            layerTitle: {
+                ref: 'layerTitle',
+                label: 'Layer Name',
+                type: 'string',
+                expression: 'optional',
+            },
+            layerType: {
+                ref: 'layerType',
+                type: 'string',
+                component: 'dropdown',
+                label: 'Layer Type',
+                options: [
+                    { value: 'marker', label: 'Marker' },
+                    // Add more layer types as needed
+                ],
+                defaultValue: 'marker',
+            },
+            fieldName: {
+                ref: 'fieldName',
+                label: 'Field',
+                type: 'string',
+                expression: 'optional',
+            },
+            latitude: {
+                ref: 'latitudeField',
+                label: 'Latitude Field',
+                type: 'string',
+                expression: 'optional',
+            },
+            longitude: {
+                ref: 'longitudeField',
+                label: 'Longitude Field',
+                type: 'string',
+                expression: 'optional',
+            },
+        },
+    };
+
+    const defaultView = {
+        label: 'Default View',
+        type: 'items',
+        items: {
+            defaultViewLat: {
+                ref: 'defaultViewLat',
+                label: 'Latitude',
+                type: 'number',
+                defaultValue: 21.4333197,
+            },
+            defaultViewLong: {
+                ref: 'defaultViewLong',
+                label: 'Longitude',
+                type: 'number',
+                defaultValue: -157.98148605,
+            },
+        },
+    };
+
     return {
         type: 'items',
         component: 'accordion',
         items: {
-            dimensions: {
-                uses: 'dimensions',
-                min: 1,
-                max: 1,
-            },
-            measures: {
-                uses: 'measures',
-                min: 1,
-                max: 1,
-            },
+            layers,
+            defaultView,
+            // dimensions: {
+            //     uses: 'dimensions',
+            //     min: 1,
+            //     max: 1,
+            // },
+            // measures: {
+            //     uses: 'measures',
+            //     min: 1,
+            //     max: 1,
+            // },
             sorting: {
                 uses: 'sorting',
             },
             settings: {
                 uses: 'settings',
             },
-            appearance: { uses: 'settings' },
         },
     };
 });
